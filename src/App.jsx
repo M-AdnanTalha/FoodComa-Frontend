23a0fa9da5ce13440d51cda68872979dc94ff559
Mohd Adnan Talha <adnan.ad7077@gmail.com>
2024-08-27 01:59:06 +0530

    Added admin addProduct

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Layout from './Layouts/Layout'
 import Home from './Pages/Home'
 import Signup from './Pages/Auth/Signup'
 import Login from './Pages/Auth/Login'
 import NotFound from './Pages/NotFound'
+import Denied from './Pages/Denied'
+import AddProduct from './Pages/Admin/Addproduct'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/denied' element={<Denied/>}/>
         <Route path='/auth/signup' element={<Signup/>}/>
         <Route path='/auth/login' element={<Login/>} />
 
+        <Route path='/admin/addProduct' element={<AddProduct/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </>
