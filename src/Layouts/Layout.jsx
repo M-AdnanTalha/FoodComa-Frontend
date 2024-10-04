5d603e614b3caa73734f6a39e8f9973864dcbaf6
Mohd Adnan Talha <adnan.ad7077@gmail.com>
2024-10-05 02:56:45 +0530

    Added redux thunk for logout and login

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,7 +1,18 @@
-import Footer from "../Components/Footer";
+import { useDispatch, useSelector } from 'react-redux';
+import Footer from '../Components/Footer';
 import Pizzalogo from '../assets/Images/pizza1.png';
+import { Link } from 'react-router-dom';
+import { logout } from '../Redux/Slices/AuthSlice';
 
+// eslint-disable-next-line react/prop-types
 function Layout({ children }) {
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
+    async function handleLogout(e) {
+        e.preventDefault();
+        dispatch(logout());
+        
+    }
     return (
         <div>
 
@@ -33,6 +44,17 @@ function Layout({ children }) {
                     </ul>
                 </div>
 
+                <div>
+                    <ul className='flex gap-4'>
+                        <li className='hover:text-[#FF9110]'>
+                            {isLoggedIn ? (
+                                <Link onClick={handleLogout}>Logout</Link>
+                            ) : (
+                                <Link to={'/auth/login'}>Login</Link>
+                            )}
+                        </li>
+                    </ul>
+                </div>
             </nav>
 
                 {children}
@@ -41,5 +63,3 @@ function Layout({ children }) {
         </div>  
     )
 }
-
-export default Layout;
