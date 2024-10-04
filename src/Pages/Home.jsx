e3e12a6642b5f64662a346afc7529fd912444058
Mohd Adnan Talha <adnan.ad7077@gmail.com>
2024-10-05 03:04:01 +0530

    Added product slice

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,8 +6,18 @@ import OrderFood from '../assets/Images/orderFood.png';
 import Pickup from '../assets/Images/pickup.png';
 import Enjoy from '../assets/Images/enjoy.png';
 import Layout from '../Layouts/Layout';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllProducts } from "../Redux/Slices/ProductSlice";
 
 function Home(){
+    const dispatch = useDispatch();
+    const { productsData } = useSelector((state) => state.product);
+    useEffect(() => {
+        // This will be called when the component mounts
+        dispatch(getAllProducts());
+    }, []);
+
    return(
     <Layout>
         <div>
@@ -126,6 +136,9 @@ function Home(){
                 </div>
             </div>
         </section>
+
+        {/* Todo: Build the ui for showing all pizzas */}
+        {productsData.map((product) => <div key={product._id}>{product.productName}</div>)}
 
         </div>
     </Layout>
